6fce3b049b16fee84f06b265987ae0927942693f
erikringwalters <erik.ring.walters@gmail.com>
2017-09-08 01:49:25 -0700

    first commit

---
+++ clayface.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx.stroke();
+=======
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx.stroke();
+>>>>>>> 51a61f3777f9532bbe5fc82ff3d3142573f3d691
